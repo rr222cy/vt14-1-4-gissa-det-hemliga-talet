79d6f7141f1f17e1f41eb1cd94835e6d02457e07
rr222cy <rr222cy@student.lnu.se>
2014-02-02 20:49:49 +0100

    Fortsatt strukturering av affärslogiklagret

--- GuessTheNumber/GuessTheNumber/Models/SecretNumber.cs
+++ GuessTheNumber/GuessTheNumber/Models/SecretNumber.cs
@@ -6,8 +6,8 @@ using System.Web;
 public enum Outcome
 {
     Indefinite,
-    Low = 1,
-    High = 100,
+    Low,
+    High,
     Correct,
     NoMoreGuesses,
     PreviousGuess
@@ -17,9 +17,47 @@ namespace GuessTheNumber.Models
 {
     public class SecretNumber
     {
-        // Defining the fields that will be used.
+        // The fields for the class.
         private int _number;
         private List<int> _previousGuesses = new List<int>();
         public const int MaxNumberOfGuesses = 8;
+
+        // The properties for the class.
+        public bool CanMakeGuess { get; }
+        public int Count { get; }
+        public int? Number { get; }
+        public Outcome Outcome { get; set; }
+        public IEnumerable<int> PreviousGuesses
+        {
+            get { return _previousGuesses; }
+        }
+
+        // The methods for the class.
+        public void Initialize()
+        {
+
+        }
+
+        public Outcome MakeGuess(int guess)
+        {
+            if (guess > _number)
+            {
+                return Outcome.High;
+            }
+            else if (guess < _number)
+            {
+                return Outcome.Low;
+            }
+            else
+            {
+                return Outcome.Correct;
+            }
+        }
+
+        // The constructors for the class.
+        public SecretNumber()
+        {
+
+        }
     }
 }
